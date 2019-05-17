00ec19f6f33adbe18f052885884ee4b503415fba
JADC362 <32496764+JADC362@users.noreply.github.com>
2019-05-16 23:22:20 -0500

    Add files via upload

---
+++ scripts/Robot5_Ejecucion.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd ~/catkin_ws/src/Robot5/scripts/
+chmod +x *.py
+cd ~/catkin_ws/
+source devel/setup.bash 
+rosrun Robot5 Robot5_ComunicacionMaster.py $(hostname -I) &
+rosrun Robot5 Robot5_Cinematica.py &
