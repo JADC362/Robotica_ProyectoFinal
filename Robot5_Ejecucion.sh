da7550e21be0802165dad94d16984caa75208fae
JADC362 <32496764+JADC362@users.noreply.github.com>
2019-05-16 23:39:26 -0500

    Delete Robot5_Ejecucion.sh

--- Robot5_Ejecucion.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd ~/catkin_ws/src/Robot5/scripts/
-chmod +x *.py
-cd ~/catkin_ws/
-source devel/setup.bash 
-rosrun Robot5 Robot5_ComunicacionMaster.py $(hostname -I) &
-rosrun Robot5 Robot5_Cinematica.py &
-rosrun Robot5 Robot5_Movimiento.py &
